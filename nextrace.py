909b336bd3c3fd0244430aeb78734d25632e5df8
ajayprasad97 <kawashakhi@gmail.com>
2023-03-03 20:41:54 -0500

    Update nextrace.py

--- nextrace.py
+++ nextrace.py
@@ -1,15 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.formula1.com/en/racing/2023.html'
+url = 'https://www.formula1.com/en/racing/2023.html'
+response = requests.get(url)
 
-response = requests.get(URL)
+soup = BeautifulSoup(response.text, 'html.parser')
+race_title = soup.find('div', {'class': 'event-title f1--xxs'}).text
+race_location = soup.find('div', {'class': 'event-place d-block'}).text
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'html.parser')
-    next_race = soup.find('div', {'class': 'f1-page-header--race'}).find('a').text.strip()
-    race_time = soup.find('div', {'class': 'f1-race-details__date'}).text.strip()
-    location = soup.find('div', {'class': 'f1-race-details__circuit'}).text.strip()
-    print(f"The next F1 race is {next_race} at {location} on {race_time}.")
-else:
-    print(f"Error {response.status_code}: {response.text}")
+print(f'Next race: {race_title} at {race_location}')
